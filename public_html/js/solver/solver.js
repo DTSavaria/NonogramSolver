494d9ff215600f8d82733535c72b034a402fc9db
DTSavaria <dtsavaria+git@gmail.com>
2016-01-20 21:14:00 -0500

    Adding some more output options before starting the actual solve algorithm.

--- public_html/js/solver/solver.js
+++ public_html/js/solver/solver.js
@@ -11,15 +11,58 @@ var Solver = function (rows, columns) {
 Solver.prototype.solve = function () {
 };
 
+Solver.prototype.puzzleToGridString = function (
+        startGrid, endGrid, startRow, endRow, startElement, endElement) {
+    var rowMatrix = this.rowsToMatrix();
+    var rowLength = rowMatrix[0].length;
+    var columnMatrix = this.columnsToMatrix();
+
+    var toReturn = startGrid;
+
+    for (var i = 0; i < columnMatrix[0].length; i++) {
+        toReturn += startRow;
+        for (var j = 0; j < rowLength; j++) {
+            toReturn += startElement + endElement;
+        }
+        columnMatrix.forEach(function (column) {
+            toReturn += startElement + column[i] + endElement;
+        });
+        toReturn += endRow;
+    }
+
+    rowMatrix.forEach(function (row) {
+        toReturn += startRow;
+        row.forEach(function (element) {
+            toReturn += startElement + element + endElement;
+        });
+        for (var i = 0; i < columnMatrix.length; i++) {
+            toReturn += startElement + endElement;
+        }
+        toReturn += endRow;
+    });
+
+    toReturn += endGrid;
+
+    return toReturn;
+};
+
+Solver.prototype.rowsToMatrix = function () {
+    return vectorToMatrix(this.rows);
+};
+
+Solver.prototype.columnsToMatrix = function () {
+    return vectorToMatrix(this.columns);
+};
+
 Solver.prototype.rowsToString = function () {
-    return this.vectorToString(this.rows, 2);
+    return vectorToString(this.rows, 2);
 };
 
 Solver.prototype.columnsToString = function () {
-    return this.vectorToString(this.columns, 2);
+    return vectorToString(this.columns, 2);
 };
 
-Solver.prototype.vectorToString = function (vector, depth) {
+function vectorToString(vector, depth) {
     var toReturn = "[";
     vector.forEach(function (entry) {
         if (entry instanceof Array && depth > 1) {
@@ -32,4 +75,23 @@ Solver.prototype.vectorToString = function (vector, depth) {
     toReturn = toReturn.slice(0, -2);
     toReturn += "]";
     return toReturn;
-};
+}
+
+function vectorToMatrix(vector) {
+    var max = 0;
+    vector.forEach(function (entry) {
+        max = Math.max(max, entry.length);
+    });
+    var matrix = [];
+    vector.forEach(function (entry) {
+        var vector = [];
+        for (var i = 0; i < max - entry.length; i++) {
+            vector.push(0);
+        }
+        entry.forEach(function (element) {
+            vector.push(element);
+        });
+        matrix.push(vector);
+    });
+    return matrix;
+}
